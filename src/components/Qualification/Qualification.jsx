791ff92759eb5eb47bd929e10991d1f0ad06cae9
luiscorreagiraldo <luiscorrea250901@gmail.com>
2023-04-01 21:25:26 -0500

    changes added

--- src/components/Qualification/Qualification.jsx
+++ src/components/Qualification/Qualification.jsx
@@ -1,21 +1,70 @@
-import React from 'react'
-import "./Qualification.css"
+import React from "react";
+import "./Qualification.css";
 
 const Qualification = () => {
   return (
     <section className="qualification section">
-        <h2 className="section__title">Qualifications</h2>
-    <div className="qualification__container container">
+      <h2 className="section__title">Qualifications</h2>
+      <div className="qualification__container container">
         <div className="qualification__tabs">
-            <div className="qualification__button button--flex">
-                <i className="uil uil-graduation-cap qualification__icon"></i>Education
-            </div>
-           
+          <div className="qualification__button qualification__active button--flex">
+            <i className="uil uil-graduation-cap qualification__icon"></i>
+            Education
+          </div>
         </div>
-    </div>
-    
-    </section>
-  )
-}
 
-export default Qualification
+        <div className="qualification__sections">
+          <div className="qualification__content">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">WEB DESIGN</h3>
+                <span className="qualification__subtitle">PLATZI</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i>2021 - 2022
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">WEB DEVELOPMENT</h3>
+                <span className="qualification__subtitle">MINTIC</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i>2022 - 2022
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">FRONTEND DEVELOPMENT</h3>
+                <span className="qualification__subtitle">META | COURSERA</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i>2022 - Present
+                </div>
+              </div>
+            </div>
+
+   
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
